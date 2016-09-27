49be0ea13363c5e1bcf23eaacea0025db30664c9
Lifelong-Study <songhua.develop@gmail.com>
2016-09-28 07:52:24 +0800

    Add UIView extension

---
+++ EliteFramework/EliteView.swift
@@ -0,0 +1,17 @@
+//
+//  EliteView.swift
+//  EliteFramework
+//
+//  Created by Lifelong-Study on 2016/9/24.
+//  Copyright © 2016年 Lifelong-Study. All rights reserved.
+//
+
+import UIKit
+
+public extension UIView {
+    
+    //
+    func renderGradientEffectsWithDirection(direction: EliteDirection, colorsArray: Array<UIColor>) {
+        self.layer.renderGradientEffectsWithDirection(direction, colorsArray: colorsArray)
+    }
+}
