a2d047311023f5c4e2ca0b9ab1dc79091735a3fe
Lifelong-Study <songhua.develop@gmail.com>
2016-09-28 08:00:09 +0800

    Add NSLayoutConstraint extension

--- EliteFramework/EliteLayoutConstraint.swift
+++ EliteFramework/EliteLayoutConstraint.swift
@@ -10,8 +10,25 @@ import UIKit
 
 public extension NSLayoutConstraint {
     
-    //!
-    public class func constraintsWithVisualFormat(_ format: String, views: [String : AnyObject]) -> [NSLayoutConstraint] {
-        return constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: views)
+    //
+    class func constraintWithView(item: UIView, attribute: NSLayoutAttribute) -> NSLayoutConstraint {
+        return constraint(item: item, attribute: attribute, offset: 0.0)
     }
+    
+    //
+    class func constraint(item: UIView, attribute: NSLayoutAttribute, offset: CGFloat) -> NSLayoutConstraint {
+        return NSLayoutConstraint(item: item, attribute: attribute, relatedBy: .equal, toItem: item.superview, attribute: attribute, multiplier: 1.0, constant: offset)
+    }
+    
+    //
+    class func constraints(withVisualFormat format: String, views: [String : AnyObject]) -> [NSLayoutConstraint] {
+        return constraints(withVisualFormat: format, options: .directionLeadingToTrailing, metrics: nil, views: views)
+    }
+    
+    //
+    class func constraints(withVisualFormat format: String, metrics: [String : Any]?, views: [String : AnyObject]) -> [NSLayoutConstraint] {
+        return constraints(withVisualFormat: format, options: .directionLeadingToTrailing, metrics: metrics, views: views)
+    }
+    
+    
 }
