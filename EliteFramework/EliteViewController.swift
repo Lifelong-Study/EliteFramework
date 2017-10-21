901dd83b90bc3bdca043e59221c8f94aafd15748
Lifelong-Study <songhua.develop@gmail.com>
2017-10-22 00:36:22 +0800

    [Other] Organize the code

--- EliteFramework/EliteViewController.swift
+++ EliteFramework/EliteViewController.swift
@@ -8,29 +8,8 @@
 
 import UIKit
 
-fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
-  switch (lhs, rhs) {
-  case let (l?, r?):
-    return l < r
-  case (nil, _?):
-    return true
-  default:
-    return false
-  }
-}
-
-fileprivate func <= <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
-  switch (lhs, rhs) {
-  case let (l?, r?):
-    return l <= r
-  default:
-    return !(rhs < lhs)
-  }
-}
-
-
 public extension UIViewController {
-
+    
     // 過場動畫
     public enum TransferAnimation {
         case push
@@ -39,38 +18,32 @@ public extension UIViewController {
     }
     
     // MARK:
-//    public func transfer(storyboard: String?, identifier: String?, animation: TransferAnimation) {
-//        transferViewController(storyboard: String?, identifier: String?, animation: TransferAnimation)
-//    }
-    
-    // MARK:
-//    public func transferViewController(storyboard: String?, identifier: String?, animation: TransferAnimation) {
-//
-//        if storyboard == nil {
-//            return ;
-//        }
-//
-//        let storyboard = UIStoryboard(name: storyboard!, bundle: nil)
-//
-//        return transferViewController(storyboard: storyboard, identifier: identifier, animation: animation)
-//    }
-    
-    // MARK:
-    public func transfer(storyboard: UIStoryboard?, identifier: String?, animation: TransferAnimation) {
-        // 防呆
-        if storyboard == nil || identifier == nil {
-            return
-        }
-        
-        transfer(viewController: storyboard!.instantiateViewController(withIdentifier: identifier!), animation: animation)
-//        transferViewController(viewController: storyboard!.instantiateViewController(withIdentifier: identifier!), animation: animation)
+    public func transfer(identifier: String, animation: TransferAnimation) {
+        transfer(viewController: storyboard?.instantiateViewController(withIdentifier: identifier), animation: animation)
     }
     
     // MARK:
-//    @available(*, deprecated, renamed: "transfer")
-//    public func transferViewController(storyboard: UIStoryboard?, identifier: String?, animation: TransferAnimation) {
-//        transfer(storyboard: UIStoryboard?, identifier: String?, animation: TransferAnimation)
-//    }
+    public func transfer(storyboard: String, identifier: String?, animation: TransferAnimation) {
+        
+        // 防呆
+        if identifier == nil {
+            return
+        }
+        
+        
+        transfer(storyboard: UIStoryboard(name: storyboard, bundle: nil), identifier: identifier, animation: animation)
+    }
+    
+    // MARK:
+    public func transfer(storyboard: UIStoryboard?, identifier: String?, animation: TransferAnimation) {
+        
+        // 防呆
+        if identifier == nil {
+            return
+        }
+        
+        transfer(viewController: storyboard?.instantiateViewController(withIdentifier: identifier!), animation: animation)
+    }
     
     // MARK:
     public func transfer(viewController: UIViewController?, animation: TransferAnimation) {
@@ -134,12 +107,12 @@ public extension UIViewController {
     }
     
     // MARK:
-    public func alertController(title: String, button: String) {
-        alertController(title: title, message: nil, button: button)
+    public func alert(title: String, button: String) {
+        alert(title: title, message: nil, button: button)
     }
     
     // MARK:
-    public func alertController(title: String, message: String?, button: String) {
+    public func alert(title: String, message: String?, button: String) {
         
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
@@ -148,5 +121,3 @@ public extension UIViewController {
         present(alertController, animated: true, completion: nil)
     }
 }
-
-//class 
