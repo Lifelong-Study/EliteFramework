27bac3a486f8a8996b16229a6d2363aa88755b38
Song-Hua, Yang <songhua.develop@gmail.com>
2020-11-08 00:49:02 +0800

    init

--- EliteFramework/EliteFramework.h
+++ EliteFramework/EliteFramework.h
@@ -15,7 +15,6 @@ FOUNDATION_EXPORT double EliteLibraryVersionNumber;
 FOUNDATION_EXPORT const unsigned char EliteLibraryVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <EliteFramework/PublicHeader.h>
-#import <EliteFramework/EliteArray.h>
 #import <EliteFramework/EliteView.h>
 #import <EliteFramework/EliteColor.h>
 #import <EliteFramework/EliteConfig.h>
